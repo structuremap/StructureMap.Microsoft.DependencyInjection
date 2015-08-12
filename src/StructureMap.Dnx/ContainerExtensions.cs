28abdb7810e4afb56bea4f85044ffdec8ea6da3b
Kristian Hellang <kristian@hellang.com>
2015-08-12 20:10:25 +0200

    Added XML docs

--- src/StructureMap.Dnx/ContainerExtensions.cs
+++ src/StructureMap.Dnx/ContainerExtensions.cs
@@ -5,6 +5,11 @@ namespace StructureMap
 {
     public static class ContainerExtensions
     {
+        /// <summary>
+        /// Populates the container using the specified service descriptors.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <param name="descriptors">The service descriptors.</param>
         public static void Populate(this IContainer container, IEnumerable<ServiceDescriptor> descriptors)
         {
             container.Configure(config => config.AddRegistry(new ServiceCollectionRegistry(descriptors)));
