48c597fcb3b53aa989cc194d48fb173a15661136
Kristian Hellang <kristian@hellang.com>
2015-10-12 15:48:50 +0200

    Added tests from official test suite

---
+++ test/StructureMap.Dnx.Tests/StructureMapContainerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Framework.DependencyInjection.Tests;
+using Microsoft.Framework.DependencyInjection.Tests.Fakes;
+
+namespace StructureMap.Dnx.Tests
+{
+    public class StructureMapContainerTests : ScopingContainerTestBase
+    {
+        protected override IServiceProvider CreateContainer()
+        {
+            var container = new Container();
+
+            container.Populate(TestServices.DefaultServices());
+
+            return container.GetInstance<IServiceProvider>();
+        }
+    }
+}
