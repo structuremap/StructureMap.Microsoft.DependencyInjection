4c93d214e2742653531226ec580e355214ee7826
Kristian Hellang <kristian@hellang.com>
2015-11-24 02:04:55 +0100

    Removed DependencyInjection submodule

--- test/StructureMap.Dnx.Tests/StructureMapContainerTests.cs
+++ test/StructureMap.Dnx.Tests/StructureMapContainerTests.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using Microsoft.Framework.DependencyInjection.Tests;
-using Microsoft.Framework.DependencyInjection.Tests.Fakes;
+﻿//using System;
+//using Microsoft.Framework.DependencyInjection.Tests;
+//using Microsoft.Framework.DependencyInjection.Tests.Fakes;
 
-namespace StructureMap.Dnx.Tests
-{
-    public class StructureMapContainerTests : ScopingContainerTestBase
-    {
-        protected override IServiceProvider CreateContainer()
-        {
-            var container = new Container();
+//namespace StructureMap.Dnx.Tests
+//{
+//    TODO: Add this test back with Microsoft.Extensions.DependencyInjection.Specification.Tests
+//    public class StructureMapContainerTests : ScopingContainerTestBase
+//    {
+//        protected override IServiceProvider CreateContainer()
+//        {
+//            var container = new Container();
 
-            container.Populate(TestServices.DefaultServices());
+//            container.Populate(TestServices.DefaultServices());
 
-            return container.GetInstance<IServiceProvider>();
-        }
-    }
-}
+//            return container.GetInstance<IServiceProvider>();
+//        }
+//    }
+//}
