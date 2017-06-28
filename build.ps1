4d076333698c25f367264080c8cd6b031ae8e6c3
Kristian Hellang <kristian.hellang@iris.no>
2017-06-28 09:55:36 +0200

    Updated build script

--- build.ps1
+++ build.ps1
@@ -5,24 +5,60 @@ function Install-Dotnet
   if(($LASTEXITCODE -ne 0) -Or ((Test-Path Env:\APPVEYOR) -eq $true))
   {
     Write-Host "Dotnet CLI not found - downloading latest version"
-    & { iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.ps1')) }
+
+    # Prepare the dotnet CLI folder
+    $env:DOTNET_INSTALL_DIR="$(Convert-Path "$PSScriptRoot")\.dotnet\win7-x64"
+    if (!(Test-Path $env:DOTNET_INSTALL_DIR))
+    {
+      mkdir $env:DOTNET_INSTALL_DIR | Out-Null
+    }
+
+    # Download the dotnet CLI install script
+    if (!(Test-Path .\dotnet\install.ps1))
+    {
+      Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.1/scripts/obtain/dotnet-install.ps1" -OutFile ".\.dotnet\dotnet-install.ps1"
+    }
+
+    # Run the dotnet CLI install
+    & .\.dotnet\dotnet-install.ps1 -Version "1.0.3"
+
+    # Add the dotnet folder path to the process.
+    Remove-PathVariable $env:DOTNET_INSTALL_DIR
+    $env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
   }
+}
+
+function Remove-PathVariable
+{
+  [cmdletbinding()]
+  param([string] $VariableToRemove)
+  $path = [Environment]::GetEnvironmentVariable("PATH", "User")
+  $newItems = $path.Split(';') | Where-Object { $_.ToString() -inotlike $VariableToRemove }
+  [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "User")
+  $path = [Environment]::GetEnvironmentVariable("PATH", "Process")
+  $newItems = $path.Split(';') | Where-Object { $_.ToString() -inotlike $VariableToRemove }
+  [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "Process")
 }
 
 function Restore-Packages
 {
     param([string] $DirectoryName)
-    & dotnet restore -v Error ("""" + $DirectoryName + """")
+    & dotnet restore -v minimal ("""" + $DirectoryName + """")
+    if($LASTEXITCODE -ne 0) { exit 1 }
 }
 
-function Test-Projects
+function Test-Project
 {
-    & dotnet test;
+    param([string] $ProjectPath)
+    & dotnet test -v minimal -c Release ("""" + $ProjectPath + """")
+    if($LASTEXITCODE -ne 0) { exit 1 }
+}
 
-    if($LASTEXITCODE -ne 0)
-    {
-      exit 3
-    }
+function Pack-Project
+{
+    param([string] $ProjectPath)
+    & dotnet pack -v minimal -c Release --output packages ("""" + $ProjectPath + """")
+    if($LASTEXITCODE -ne 0) { exit 1 }
 }
 
 ########################
@@ -35,15 +71,12 @@ Push-Location $PSScriptRoot
 Install-Dotnet
 
 # Package restore
-Write-Host "Running package restore"
-Get-ChildItem -Path . -Filter *.xproj -Recurse | ForEach-Object { Restore-Packages $_.DirectoryName }
+Get-ChildItem -Path . -Filter *.csproj -Recurse | ForEach-Object { Restore-Packages $_.DirectoryName }
 
 # Tests
-Write-Host "Running tests"
-Get-ChildItem -Path .\test -Filter *.xproj -Recurse | ForEach-Object {
-    Push-Location $_.DirectoryName
-    Test-Projects
-    Pop-Location
-}
+Get-ChildItem -Path .\test -Filter *.csproj -Recurse | ForEach-Object { Test-Project $_.FullName }
+
+# Pack
+Get-ChildItem -Path .\src -Filter *.csproj -Recurse | ForEach-Object { Pack-Project $_.FullName }
 
 Pop-Location
