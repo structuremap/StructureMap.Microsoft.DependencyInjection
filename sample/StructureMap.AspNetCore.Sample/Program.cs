863de8b49cd5f5677f915d1314b963e1459daba2
Kristian Hellang <kristian@hellang.com>
2019-02-11 08:53:05 +0100

    Added back a fully working sample

---
+++ sample/StructureMap.AspNetCore.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace StructureMap.AspNetCore.Sample
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStructureMap() // Add support for StructureMap
+                .UseStartup<Startup>();
+    }
+}
